e7b69d95af746aef22476b3b9f578a2813540123
Judah Hunger <judah.hunger@gmail.com>
2018-04-28 11:20:18 -0700

    changes to indext and routes need to fix

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -7,17 +7,10 @@ page('/*', (ctx, next) => {
 next();
 });
 
-<<<<<<< HEAD
+
 page('/', app.restaurantView.initIndexView);
 page('/form', () => app.restaurantView.initFormView());
 page('/display', () => app.restaurant.fetch(app.restaurantView.initDisplayView));
-
-
-page.start();
-=======
-page('/', () => $('#home-view').fadeToggle(1500));
-page('/display', () => app.restaurant.fetch(app.test.initView));
-page('/test', () => app.form.init());
 page('/preferences', () => app.preferences.initView());
+
 page.start();
->>>>>>> 4f2b884f00305a1ce0e0cb1b5ee51cc8daeec888
