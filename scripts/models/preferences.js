de70ac0b4d3245c42a557eb579a8a40cd3e27ad9
Aaron <aaronmeade9292@gmail.com>
2018-04-28 17:01:46 -0700

    working on preferences

--- scripts/models/preferences.js
+++ scripts/models/preferences.js
@@ -4,8 +4,9 @@
 //
   //get the values of the buttons and tore into an array and insert into DB. if the value of the array exists highlight the corresponding button.
   var preferenceArray = [];
-  $('.choice').on('click', (e) => {
-    if(!preferenceArray.includes(e.target.value)){
-      preferenceArray.push(e.target.value);
+  $('.save-preferences').on('click', (e) => {
+    console.log('workk');
+    // if(!preferenceArray.includes(e.target.value)){
+    //   preferenceArray.push(e.target.value);
     }
   });
