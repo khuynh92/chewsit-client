09755777fa024b0bcbbc2ca50121c562e5108d2e
Judah Hunger <judah.hunger@gmail.com>
2018-04-30 18:08:17 -0700

    worked on preference button and working as intended.

--- scripts/models/preferences.js
+++ scripts/models/preferences.js
@@ -5,11 +5,15 @@
   //get the values of the buttons and tore into an array and insert into DB. if the value of the array exists highlight the corresponding button.
   var preferenceArray = [];
   $('.save-preferences').on('click', (e) => {
-    $('.choice:checked').each(function(){ console.log($(this).val())}) 
-   
-    // NEEED TO WORK BELOW
-    if(!preferenceArray.includes($('.choice:checked')).val()){
-      preferenceArray.push($('choice:checked').val());
-    }
-      
+    $('.choice:checked').each(function(){ console.log($(this).val())
+      if(!preferenceArray.includes($(this).val())){
+        preferenceArray.push($(this).val());
+      }
+    }) 
+    $('.choice').not(':checked').each(function(){ console.log(`unchecked: `, $(this).val())
+    if(preferenceArray.includes($(this).val())){
+      preferenceArray.splice(preferenceArray.indexOf($(this).val(), 1))
+        }
+      }) 
     })
+   
