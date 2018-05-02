e5779c30a7d960be756b15e50d3a32ff7167a0cf
Khoa Huynh <khoawin92@gmail.com>
2018-05-02 00:07:49 -0700

    enabled cors

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 const sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const PORT = process.env.PORT;
+const cors = require('cors');
 const app = express();
 
+app.use(cors({ origin: '*' }));
 app.use(sslRedirect());
 app.use(express.static('./public'));
 app.use((req, res) => res.sendFile('404.html', {root: './public'}));
