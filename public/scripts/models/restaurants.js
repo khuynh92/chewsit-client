1c4515a993e156e1dccdc6d336f4be8cdee3260c
Khoa Huynh <khoawin92@gmail.com>
2018-05-02 00:09:43 -0700

    added https to production api

--- public/scripts/models/restaurants.js
+++ public/scripts/models/restaurants.js
@@ -3,7 +3,7 @@
 var app = app || {};
 const ENV = {};
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'http://chewsit-server.herokuapp.com';
+ENV.productionApiUrl = 'https://chewsit-server.herokuapp.com';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
