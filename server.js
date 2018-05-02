88fc47828af7aa73872094f97c0ec72fbdb2257d
Khoa Huynh <khoawin92@gmail.com>
2018-05-01 23:58:26 -0700

    redirect to https

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
 require('dotenv').config();
+const sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(sslRedirect());
 app.use(express.static('./public'));
 app.use((req, res) => res.sendFile('404.html', {root: './public'}));
 app.listen(PORT, () => console.log('Listening on PORT', PORT));
