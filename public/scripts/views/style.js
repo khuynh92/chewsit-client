6cc034aaaaa0836e9d1e02e9cd538aa74464d368
Khoa Huynh <khoawin92@gmail.com>
2018-05-05 10:32:22 -0700

    finished about me

--- public/scripts/views/style.js
+++ public/scripts/views/style.js
@@ -6,11 +6,11 @@ $('.nav-container').on('click', function(){
   $('.nav-ul').toggleClass('sidenav').toggleClass('hide-nav')
 })
 
-$('html').click(function() {
+$('html').on('click', (function() {
   $('.nav-container').removeClass("change")
   $('.nav-ul').removeClass('sidenav').addClass('hide-nav')
-});
+}));
 
-$('.nav-container').click(function(event){
+$('.nav-container').on('click', (function(event){
     event.stopPropagation();
-});
+}));
