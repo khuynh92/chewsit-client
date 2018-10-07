6553b28780a24d5e5b9b47c4eb1ed63c68ef4c01
Khoa Huynh <khoawin92@gmail.com>
2018-10-06 20:48:52 -0700

    changed position of about button

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -69,13 +69,13 @@ const styles = theme => {
     about: {
       [theme.breakpoints.between('xs', 'sm')]: {
         textDecoration: 'none',
-        position: 'absolute',
-        bottom: '12vh',
+        position: 'fixed',
+        top: '78vh',
       },
       [theme.breakpoints.between('sm', 'lg')]: {
         textDecoration: 'none',
         position: 'fixed',
-        bottom: '2vh',
+        top: '90vh',
       },
     },
   });
