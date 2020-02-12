3361115991932c7ed19c08d374566abe47eff96f
Khoa Huynh <khoawin92@gmail.com>
2020-02-12 02:16:28 -0800

    fix placeholder text

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -196,7 +196,7 @@ class Dashboard extends Component {
                 value={this.state.locationForm}
                 onChange={this.handleChange('locationForm')}
                 margin="normal"
-                placeholder="seattle | 98125 | microsoft"
+                placeholder="seattle | 98125 | Microsoft"
               />
               {/* } */}
 
--- src/components/guest/GuestDashboard.js
+++ src/components/guest/GuestDashboard.js
@@ -191,7 +191,7 @@ class Dashboard extends Component {
               value={this.state.locationForm}
               onChange={this.handleChange('locationForm')}
               margin="normal"
-              placeholder="username"
+              placeholder="seattle | 98125 | Microsoft"
             />
             {/* } */}
 
